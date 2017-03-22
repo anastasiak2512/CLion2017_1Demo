f2edc5f79c8f538c04c3f5a06762227482599cb7
Anastasia Kazakova <anastasia.kazakova@jetbrains.com>
2017-03-22 16:41:44 +0300

    cleaning the code sample

--- CPP14_support/return_type_deduction.cpp
+++ CPP14_support/return_type_deduction.cpp
@@ -7,15 +7,14 @@
 
 auto prev_vec() {
     std::vector<double> vec;
-
     vec.push_back(2016.3);
     return vec;
 }
 
 void cur_vec() {
-    auto v = prev_vec();
-
-    v.push_back(2017.1);
+    auto vec = prev_vec();
+    vec.push_back(2017.1);
+    //..
 }
 
 
